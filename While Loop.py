36571c9c2572278424684cd102ecf014a2da0466
Bernard Ian Santella <santellabernardian@gmail.com>
2024-11-12 13:23:27 +0400

    Create a while loop activity

---
+++ While Loop.py
@@ -0,0 +1,12 @@
+number = int(input("Enter a number: "))
+while number >= 0:
+    print(number)
+    number -= 1
+##
+n = -2
+while n < 5: 
+    print(f'Inside the loop, the value of n is (n).')
+    n += 5 #Increment is +
+           #Decrement is - 
+
+
