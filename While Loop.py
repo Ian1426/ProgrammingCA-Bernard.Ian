e8f7648eca61fadb63b577792aafe05a78402b7e
Bernard Ian Santella <santellabernardian@gmail.com>
2024-11-12 13:37:03 +0400

    Update

--- While Loop.py
+++ While Loop.py
@@ -8,5 +8,12 @@ while n < 5:
     print(f'Inside the loop, the value of n is (n).')
     n += 5 #Increment is +
            #Decrement is - 
-
-
+##
+total = 0
+for s in range(8,1):
+    total += 1
+print("Sum:", total)
+##
+word = input("Enter a string: ")
+for char in word:
+    print(char)
