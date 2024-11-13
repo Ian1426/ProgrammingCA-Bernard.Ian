d5fa7e12906ee9c7065a5011398b2f953a6cb0a2
Bernard Ian Santella <santellabernardian@gmail.com>
2024-11-13 14:15:35 +0400

    Folder
    Arranged a folder for all Class activities

---
+++ For loop/For loop.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+for s in range(1, 11):
+    print(f"{num} x {s} = {num * s} ")
+num = int(input("Enter a number: "))
+for s in range(0, 50):
+    print(f"{num} x {s} = {num * s} ")
